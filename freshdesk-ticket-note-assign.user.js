e8e053f6ef22b9cb9d6ac8c7d89bc537a2fc5b31
jimcheung <hi.jinhu.zhang@gmail.com>
2021-10-13 12:35:21 +0800

    feat: add note assign to ticket

---
+++ freshdesk-ticket-note-assign.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Freshdesk Ticket Note Assign
+// @version     0.1.0
+// @description A userscript that adds agent assignment to note
+// @license     MIT
+// @author      Jim Cheung
+// @namespace   https://github.com/jinhucheung
+// @include     https://*.freshdesk.com/a/tickets/*
+// @run-at      document-idle
+// @grant       none
+// @icon        https://raw.githubusercontent.com/jinhucheung/freshdesk-userscripts/master/src/images/freshdesk.ico
+// @updateURL   https://raw.githubusercontent.com/jinhucheung/freshdesk-userscripts/master/freshdesk-ticket-note-assign.user.js
+// @downloadURL https://raw.githubusercontent.com/jinhucheung/freshdesk-userscripts/master/freshdesk-ticket-note-assign.user.js
+// ==/UserScript==
+(function() {
+  "use strict";
+  console.log('aa')
+})()
